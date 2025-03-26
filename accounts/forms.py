7b2994853f9bd5609d9d7a25e1c355e1049ebb51
seunghyun <tmdgus_skfk@naver.com>
2025-03-26 13:36:50 +0900

    login 기능구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,12 @@
 from .models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta():
         model = User
         # fields = '__all__'
         fields = ('username', )
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass
         
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup(request):
@@ -16,3 +17,19 @@ def signup(request):
     }
 
     return render(request, 'signup.html', context)
+
+def login(request):
+    if request.method =='POST': 
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            print(form.get_user())
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+
+    else:
+        form = CustomAuthenticationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
