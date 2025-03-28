9d9c18bf8a5b63a46ed500cb30984508261ef806
seunghyun <tmdgus_skfk@naver.com>
2025-03-28 12:21:24 +0900

    user profile 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from .models import User
 
 # Create your views here.
 def signup(request):
@@ -45,3 +46,10 @@ def logout(request):
     auth_logout(request)
     return redirect('accounts:login')
     
+def profile(request, username):
+    user_profile = User.objects.get(username=username)
+    
+    context = {
+        'user_profile': user_profile,
+    }
+    return render(request, 'profile.html', context)
